8c2429aa5432fadc3a27fdaa2cce0ead647f9795
aybike <aybikesayar@gmail.com>
2023-08-03 01:16:56 +0300

    register done

--- app/pages/register/page.tsx
+++ app/pages/register/page.tsx
@@ -1,53 +1,101 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import RegisterPic from "./register.png";
+import { useState } from "react";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/firebase/config";
+import Loading from "@/components/Loader";
+import { useRouter } from "next/navigation";
 type Props = {};
 
 const Login = (props: Props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setCpassword] = useState("");
+  const [isloading, setİsLoading] = useState(false);
+  const router = useRouter();
+
+  const registerUser = (e: any) => {
+    e.preventDefault();
+    if (password !== cPassword) {
+      toast.error("Passwords do not match");
+    }
+
+    setİsLoading(true);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        setİsLoading(false);
+        toast.success("Registiretion Succesfull...");
+        router.replace("/pages/login");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+        setİsLoading(false);
+      });
+    console.log(email, password, cPassword);
+  };
+
   return (
-    <div className="flex justify-evenly mt-10">
-      <div className="bg-white  border-gray-500 shadow-xl">
-        <h2 className="text-center text-orange-500 font-semibold text-3xl p-1">
-          Register
-        </h2>
+    <>
+      <ToastContainer />
+      {isloading && <Loading />}
+      <div className="flex justify-evenly mt-10">
+        <div className="bg-white  border-gray-500 shadow-xl">
+          <h2 className="text-center text-orange-500 font-semibold text-3xl p-1">
+            Register
+          </h2>
 
-        <form className="text-gray-500">
-          <div className="">
-            <input
-              className="w-full mb-1 border-gray-500 border-2 p-1"
-              type="text"
-              placeholder="Email"
-              required
-            />
-            <input
-              className="w-full mb-1 border-gray-500 border-2 p-1"
-              type="password"
-              placeholder="Password"
-              required
-            />
-            <input
-              className="w-full mb-1 border-gray-500 border-2 p-1"
-              type="password"
-              placeholder="Confirm Password"
-              required
-            />
-            <button
-              className="w-full mb-1 border-gray-500 border-2 bg-sky-600 p-1 text-white"
-              type="submit"
-            >
-              Register
-            </button>
-          </div>
-        </form>
+          <form className="text-gray-500" onSubmit={registerUser}>
+            <div className="">
+              <input
+                className="w-full mb-1 border-gray-500 border-2 p-1"
+                type="text"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                className="w-full mb-1 border-gray-500 border-2 p-1"
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <input
+                className="w-full mb-1 border-gray-500 border-2 p-1"
+                type="password"
+                placeholder="Confirm Password"
+                required
+                value={cPassword}
+                onChange={(e) => setCpassword(e.target.value)}
+              />
+              <button
+                className="w-full mb-1 border-gray-500 border-2 bg-sky-600 p-1 text-white"
+                type="submit"
+              >
+                Register
+              </button>
+            </div>
+          </form>
 
-        <span className="flex justify-center text-gray-600">
-          <p>Already an account?</p>
-          <Link href="/pages/login">Login</Link>
-        </span>
+          <span className="flex justify-center text-gray-600">
+            <p>Already an account?</p>
+            <Link href="/pages/login">Login</Link>
+          </span>
+        </div>
+        <Image src={RegisterPic} alt="login" width={400} height={500} />
       </div>
-      <Image src={RegisterPic} alt="login" width={400} height={500} />
-    </div>
+    </>
   );
 };
 
