ab7f98048af5c9478b894132ea178c952137d223
aybike <aybikesayar@gmail.com>
2023-08-12 22:23:01 +0300

    local storAGE DONE

--- app/pages/cart/page.tsx
+++ app/pages/cart/page.tsx
@@ -61,109 +61,122 @@ const Cart = (props: Props) => {
       <h2 className="text-orange-600 text-3xl text-center font-bold m-5">
         Shopping Cart
       </h2>
-
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left text-gray-500 ">
-          <thead className="text-xs text-gray-700 uppercase bg-orange-200 ">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Product
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Quantity
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Total
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Actions
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredArr?.map((item: any, index: number) => (
-              <tr className="bg-orange-100 border-b " key={index}>
-                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
-                  {item.title}
-                  <Image
-                    src={item?.image}
-                    alt={item?.title}
-                    width={50}
-                    height={60}
-                    className="mt-5"
-                    priority={true}
-                  />
-                </td>
-                <td className="px-6 py-4">{item.price}TL</td>
-                <td className=" flex justify-around pt-14 ">
-                  <FaMinus
-                    style={{ height: "1rem" }}
-                    onClick={() => handleDecrease(item)}
-                  />
-                  <p>
-                    {
+      {cartItem?.length ? (
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-sm text-left text-gray-500 ">
+            <thead className="text-xs text-gray-700 uppercase bg-orange-200 ">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Product
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Price
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Quantity
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Total
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredArr?.map((item: any, index: number) => (
+                <tr className="bg-orange-100 border-b " key={index}>
+                  <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                    {item.title}
+                    <Image
+                      src={item?.image}
+                      alt={item?.title}
+                      width={50}
+                      height={60}
+                      className="mt-5"
+                      priority={true}
+                    />
+                  </td>
+                  <td className="px-6 py-4">{item.price}TL</td>
+                  <td className=" flex justify-around pt-14 ">
+                    <FaMinus
+                      style={{ height: "1rem" }}
+                      onClick={() => handleDecrease(item)}
+                    />
+                    <p>
+                      {
+                        //@ts-ignore
+                        (cartItem?.filter((i) => i?.id === item?.id)).length
+                      }
+                    </p>
+                    <FaPlus
+                      style={{ height: "1rem" }}
+                      onClick={() => handleIncrease(item)}
+                    />
+                  </td>
+                  <td className="px-6 py-4">
+                    {(
+                      item?.price *
                       //@ts-ignore
                       (cartItem?.filter((i) => i?.id === item?.id)).length
-                    }
-                  </p>
-                  <FaPlus
-                    style={{ height: "1rem" }}
-                    onClick={() => handleIncrease(item)}
-                  />
-                </td>
-                <td className="px-6 py-4">
-                  {(
-                    item?.price *
-                    //@ts-ignore
-                    (cartItem?.filter((i) => i?.id === item?.id)).length
-                  ).toFixed(2)}
-                  TL
-                </td>
-                <td className="px-6 py-4">
-                  <FaTrash
-                    size={25}
-                    onClick={() => handleDelete(item)}
-                  ></FaTrash>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <div className="flex justify-between">
-          <button
-            className="bg-orange-500 rounded-md p-2  h-10"
-            onClick={() => handleClear()}
+                    ).toFixed(2)}
+                    TL
+                  </td>
+                  <td className="px-6 py-4">
+                    <FaTrash
+                      size={25}
+                      onClick={() => handleDelete(item)}
+                    ></FaTrash>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="flex justify-between">
+            <button
+              className="bg-orange-500 rounded-md p-2  h-10"
+              onClick={() => handleClear()}
+            >
+              Clear Cart
+            </button>
+            <Link href="/" className="text-black text-base flex">
+              <FaArrowCircleLeft size={25} /> Continue Shopping
+            </Link>
+          </div>
+          <div className=" bg-orange-200 text-black w-1/2 p-5 mt-5 ">
+            <div className="flex justify-between">
+              <h2> Subtotal({cartCount} items):</h2>
+
+              <p>
+                {cartItem
+                  //@ts-ignore
+                  ?.reduce((acc, current) => acc + current?.price, 0)
+                  .toFixed(2)}
+                TL
+              </p>
+            </div>
+
+            <div className="mt-5">
+              <p>Taxes and Shipping calculated at checkout</p>
+              <button className="bg-orange-500 rounded-md p-2 w-full h-10">
+                CheckOut
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <p className="flex justify-center content-center text-black font-semibold">
+            Your Cart is empty
+          </p>
+          <Link
+            href="/"
+            className="text-black text-base flex  justify-center content-center"
           >
-            Clear Cart
-          </button>
-          <Link href="/" className="text-black text-base flex">
             <FaArrowCircleLeft size={25} /> Continue Shopping
           </Link>
         </div>
-        <div className=" bg-orange-200 text-black w-1/2 p-5 mt-5 ">
-          <div className="flex justify-between">
-            <h2> Subtotal({cartCount} items):</h2>
-
-            <p>
-              {cartItem
-                //@ts-ignore
-                ?.reduce((acc, current) => acc + current?.price, 0)
-                .toFixed(2)}
-              TL
-            </p>
-          </div>
-
-          <div className="mt-5">
-            <p>Taxes and Shipping calculated at checkout</p>
-            <button className="bg-orange-500 rounded-md p-2 w-full h-10">
-              CheckOut
-            </button>
-          </div>
-        </div>
-      </div>
+      )}
     </>
   );
 };
--- app/redux/features/cartSlice.tsx
+++ app/redux/features/cartSlice.tsx
@@ -1,29 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 type initialState = {
-  cartItem: {
-    item: {
-      image: any;
-      title: any;
-      price: any;
-      id?: any;
-    };
-  };
+  cartItem: Array<any>;
   cartCount: string;
 };
 
 const initialState = {
   cartItem:
-    // (typeof window !== "undefined" &&
-    //   //@ts-ignore
-    //   JSON.parse(localStorage.getItem("cart"))) ||
-    [],
-
-  cartCount:
-    // (typeof window !== "undefined" &&
-    //   //@ts-ignore
-    //   JSON.parse(localStorage.getItem("cart"))?.length) ||
-    "0",
+    //@ts-ignore
+    JSON.parse(localStorage.getItem("cart")) || [],
+  //@ts-ignore
+  cartCount: JSON.parse(localStorage.getItem("cartCount")) || 0,
 };
 
 const cartSlice = createSlice({
@@ -34,50 +21,48 @@ const cartSlice = createSlice({
       //@ts-ignore
       state.cartItem = [...state.cartItem, payload];
       console.log("cartItem", payload);
-      // if (typeof window !== "undefined") {
-      //   localStorage.setItem("cart", JSON.stringify(state.cartItem));
-      // }
+
+      localStorage.setItem("cart", JSON.stringify(state.cartItem));
     },
     setCartCount: (state) => {
       //@ts-ignore
 
       state.cartCount = state.cartItem.length;
       console.log("cartCount", state.cartItem.length);
-      // if (typeof window !== "undefined") {
-      //   localStorage.setItem("cartCount", JSON.stringify(state.cartCount));
-      // }
+
+      //@ts-ignore
+      localStorage.setItem("cartCount", JSON.stringify(state.cartCount));
     },
 
     removeItemFromCart: (state, { payload }) => {
       state.cartItem = state.cartItem.filter(
         (item: any) => item?.id != payload.id
       );
-      // if (typeof window !== "undefined") {
-      //   localStorage.setItem("cart", JSON.stringify(state.cartItem));
-      // }
+
+      localStorage.setItem("cart", JSON.stringify(state.cartItem));
+
       //@ts-ignore
 
       state.cartCount = state.cartItem.length;
-      // if (typeof window !== "undefined") {
-      //   localStorage.setItem("cartCount", JSON.stringify(state.cartCount));
-      // }
+
+      //@ts-ignore
+      localStorage.setItem("cartCount", JSON.stringify(state.cartCount));
     },
     clearAll: (state) => {
       state.cartItem = [];
-      // if (typeof window !== "undefined") {
-      //   localStorage.setItem("cart", JSON.stringify(state.cartItem));
-      // }
 
+      localStorage.setItem("cart", JSON.stringify(state.cartItem));
+
+      //@ts-ignore
       state.cartCount = "";
-      // if (typeof window !== "undefined") {
-      //   localStorage.setItem("cartCount", JSON.stringify(state.cartCount));
-      // }
+
+      //@ts-ignore
+      localStorage.setItem("cartCount", JSON.stringify(state.cartCount));
     },
     setCartDecrease: (state, { payload }) => {
       state.cartItem = payload;
-      // if (typeof window !== "undefined") {
-      //   localStorage.setItem("cart", JSON.stringify(state.cartItem));
-      // }
+
+      localStorage.setItem("cart", JSON.stringify(state.cartItem));
     },
   },
 });
